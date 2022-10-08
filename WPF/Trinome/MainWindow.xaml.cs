e8129dce67b7f0c869d6718027a0636585a8717a
Extrymes <simon.abras@gmail.com>
2022-10-08 16:48:11 +0200

    Début trinome

---
+++ WPF/Trinome/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trinome
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            primaryButton.MouseEnter += new MouseEventHandler(Button_MouseEnter);
+            primaryButton.MouseLeave += new MouseEventHandler(Button_MouseLeave);
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            button.Background = Brushes.Gray;
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            button.Background = Brushes.LightGray;
+        }
+    }
+}
