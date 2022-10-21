9891d9a3d8bdca67a284d4dae7a00fe6eb1b1a43
Extrymes <simon.abras@gmail.com>
2022-10-21 15:29:08 +0200

    update

--- POO/ACT3/Program.cs
+++ POO/ACT3/Program.cs
@@ -4,13 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Elephant elephant1 = new Elephant("Zazou", 35);
-            Elephant elephant2 = new Elephant("Titi", 40);
+            Elephant[] elephants = new Elephant[2];
+            elephants[0] = new Elephant("Zazou", 35);
+            elephants[1] = new Elephant("Titi", 40);
 
-            Console.WriteLine("Entrez 1 pour afficher les attributs de " + elephant1.GetName());
-            Console.WriteLine("Entrez 2 pour afficher les attributs de " + elephant2.GetName());
+            Console.WriteLine("Entrez 1 pour afficher les attributs de " + elephants[0].GetName());
+            Console.WriteLine("Entrez 2 pour afficher les attributs de " + elephants[1].GetName());
             Console.WriteLine("Entrez 3 pour permuter les références pointant vers ces objets et afficher les caractéristiques de chaque éléphant");
-            Console.WriteLine("Entrez 4 pour transférer un message de " + elephant1.GetName() + " à " + elephant2.GetName());
+            Console.WriteLine("Entrez 4 pour transférer un message de " + elephants[0].GetName() + " à " + elephants[1].GetName());
+            Console.WriteLine("Entrez 5 pour afficher l'éléphant qui a la plus grande oreille");
 
             while(true)
             {
@@ -18,20 +20,34 @@
                 switch(action)
                 {
                     case "1":
-                        Console.WriteLine(elephant1.GetAttributes());
+                        Console.WriteLine(elephants[0].GetAttributes());
                         break;
                     case "2":
-                        Console.WriteLine(elephant2.GetAttributes());
+                        Console.WriteLine(elephants[1].GetAttributes());
                         break;
                     case "3":
-                        Elephant save = elephant1;
-                        elephant1 = elephant2;
-                        elephant2 = save;
-                        Console.WriteLine(elephant1.GetAttributes());
-                        Console.WriteLine(elephant2.GetAttributes());
+                        Elephant save = elephants[0];
+                        elephants[0] = elephants[1];
+                        elephants[1] = save;
+                        Console.WriteLine(elephants[0].GetAttributes());
+                        Console.WriteLine(elephants[1].GetAttributes());
                         break;
                     case "4":
-                        Console.WriteLine(elephant1.SendMessage("test", elephant2));
+                        Console.WriteLine(elephants[0].SendMessage("test", elephants[1]));
+                        break;
+                    case "5":
+                        int index = 0;
+                        uint maxValue = 0;
+                        for(int i = 0; i < elephants.Length; i++)
+                        {
+                            uint n = elephants[i].GetSizeEars();
+                            if(n > maxValue)
+                            {
+                                index = i;
+                                maxValue = n;
+                            }
+                        }
+                        Console.WriteLine(elephants[index].GetAttributes());
                         break;
                 }
             }
